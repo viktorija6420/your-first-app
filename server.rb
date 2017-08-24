8735229060846e339ad7fe80099f9eef7e3adea8
viktorija6420 <viktorija.gjorgjievska@gmail.com>
2017-08-24 16:38:09 -0400

    finished make your own app exercise

--- server.rb
+++ server.rb
@@ -11,6 +11,21 @@ get '/portfolio' do
   erb :gallery
 end
 
+get '/gallery' do
+  redirect to('/portfolio')
+end
+
 get '/about' do
+  @skills = ['git', 'HTML', 'CSS', 'Ruby']
+  @interests = ['cats', 'art', 'music', 'films', 'coffee']
   erb :about_me
+end
+
+get '/favourites' do
+  @fav_links
+  erb :favourites
+end
+
+get '/' do
+  redirect to('/home')
 end
